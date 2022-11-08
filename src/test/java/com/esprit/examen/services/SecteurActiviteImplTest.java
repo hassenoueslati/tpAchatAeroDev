449acdc665bac2247df1b57866dfb292d60ff2ba
OueslatiHassen <hassen.oueslati@esprit.tn>
2022-11-08 22:26:49 +0100

    Update SecteurActivite JUnit Test

--- src/test/java/com/esprit/examen/services/SecteurActiviteImplTest.java
+++ src/test/java/com/esprit/examen/services/SecteurActiviteImplTest.java
@@ -1,4 +1,4 @@
-/*package com.esprit.examen.services;
+package com.esprit.examen.services;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -60,4 +60,3 @@ public class SecteurActiviteImplTest {
 
 	
 }
-*/
