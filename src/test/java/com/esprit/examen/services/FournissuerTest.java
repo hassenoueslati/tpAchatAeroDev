e4a6ec1051833f1aac1b1026acabec118a62e959
Mohamed Nalouti <48924209+mednal@users.noreply.github.com>
2022-11-10 00:00:40 +0100

    test upade

--- src/test/java/com/esprit/examen/services/FournissuerTest.java
+++ src/test/java/com/esprit/examen/services/FournissuerTest.java
@@ -21,40 +21,40 @@ public class FournissuerTest {
 	IFournisseurService fs;
 	 
 	 @Test
-	    void retrieveAllSecteurActivites() {
-	    	List<Fournisseur> SecteurActivites = fs.retrieveAllFournisseurs();
-	        Assertions.assertEquals(0, SecteurActivites.size());
+	    void retrieveAllFournisseur() {
+	    	List<Fournisseur> Fournisseurs = fs.retrieveAllFournisseurs();
+	        Assertions.assertEquals(0, Fournisseurs.size());
 	    }
 	 
 	 @Test
-	    public void testAddSecteurActivite(){
-	    List<Fournisseur> SecteurActivites = fs.retrieveAllFournisseurs();
-	    int expected = SecteurActivites.size();
-	    Fournisseur s = new Fournisseur("123136","banque");
+	    public void testAddFournisseur(){
+	    List<Fournisseur> Fournisseurs = fs.retrieveAllFournisseurs();
+	    int expected = Fournisseurs.size();
+	    Fournisseur f = new Fournisseur("123136","banque");
 
-	    Fournisseur savedSecteurActivite= fs.addFournisseur(s);
+	    Fournisseur saveFournisseur= fs.addFournisseur(f);
 	    assertEquals(expected+1, fs.retrieveAllFournisseurs().size());
-	    assertNotNull(savedSecteurActivite.getLibelle());
-	    fs.deleteFournisseur(savedSecteurActivite.getIdFournisseur());
+	    assertNotNull(saveFournisseur.getLibelle());
+	    fs.deleteFournisseur(saveFournisseur.getIdFournisseur());
 
 	    }
 	 
 	 @Test
-	    public void testUpdateProduit() {
-		 Fournisseur f = new Fournisseur("123136","banque");
-		Fournisseur savedSecteurActivite= fs.addFournisseur(f);
-		savedSecteurActivite.setLibelle("ADidas");
-	    fs.updateFournisseur(savedSecteurActivite);
-	    assertEquals(f.getLibelle(),savedSecteurActivite.getLibelle());
-	    fs.deleteFournisseur(savedSecteurActivite.getIdFournisseur());
+	    public void testUpdateFournisseur() {
+		 Fournisseur s = new Fournisseur("123136","banque");
+		 Fournisseur saveFournisseur= fs.addFournisseur(s);
+		saveFournisseur.setLibelle("64654654");
+	    fs.updateFournisseur(saveFournisseur);
+	    assertEquals(s.getLibelle(),saveFournisseur.getLibelle());
+	    fs.deleteFournisseur(saveFournisseur.getIdFournisseur());
 	    }
 
 	    @Test
-	    public void testDeleteProduit() {
-	    Fournisseur f = new Fournisseur("123136","banque");
-		Fournisseur savedSecteurActivite= fs.addFournisseur(f);
-		fs.deleteFournisseur(savedSecteurActivite.getIdFournisseur());
-		assertNotNull(savedSecteurActivite.getIdFournisseur());
+	    public void testDeleteFournisseur() {
+	    	Fournisseur s = new Fournisseur("123136","banque");
+	    	Fournisseur saFournisseur= fs.addFournisseur(s);
+		fs.deleteFournisseur(saFournisseur.getIdFournisseur());
+		assertNotNull(saFournisseur.getIdFournisseur());
 
 	    }
 
