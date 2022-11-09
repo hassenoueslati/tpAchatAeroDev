c52afbfc1c9f3f580dd03fcb428247c4a5088221
louayjeddou <48138944+0Stain@users.noreply.github.com>
2022-11-09 21:51:52 +0100

    Update SpringFoxSwaggerConfig.java

--- src/main/java/com/esprit/examen/SpringFoxSwaggerConfig.java
+++ src/main/java/com/esprit/examen/SpringFoxSwaggerConfig.java
@@ -59,7 +59,7 @@ public class SpringFoxSwaggerConfig {
 		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
 		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
 		authorizationScopes[0] = authorizationScope;
-		return Arrays.asList(new SecurityReference("Bearer", authorizationScopes)); 
+		return Arrays.asList(new SecurityReference("Bearer", authorizationScopes));
 
 	}
 }
